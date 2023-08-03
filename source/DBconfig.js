d2d3b56c702eb7eb97e5b0cb2fb67a0b49e61af6
Cesar Maquera <cmaquerat@gmail.com>
2023-08-03 12:30:44 +0000

    Actualizacion de lectura de data

---
+++ source/DBconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user: "sa",
+    password: "CMaquera@123456",
+    server: "db-file",
+    database: "RCC_DIST",
+    options: {
+        enableArithAbort: true,
+        trustServerCertificate: true,
+    },
+};
+
