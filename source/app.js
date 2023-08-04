8a1ae9e7054f9bbdea82fdda19eba20e30972028
Cesar Maquera <cmaquerat@gmail.com>
2023-08-04 12:38:23 +0000

    Agregar logs de errores

--- source/app.js
+++ source/app.js
@@ -20,7 +20,7 @@ select codigo_cui from [dbo].[int_datosabiertos_mef_cui]
 union all
 select codigo_cui from [dbo].[int_datosabiertos_mef_puente]`
 
-const sql_head = 'INSERT INTO ' + utils.table_fields.join(', ') 
+const sql_head = 'INSERT INTO [dbo].[int_datosabiertos_mef_general_2023] (' + utils.table_fields.join(', ') + ') VALUES '
 
 function mainFunction() {
 
@@ -33,160 +33,61 @@ function mainFunction() {
         request.query(query_valid_codes, (err, valid_codes_data) => {
             console.log("Tiempo inicio proceso : "+utils.getDateTimeString())
             if(err) console.log(err)
-            else console.log('Lectura de codigos validos exitos...')
+            else console.log('Lectura de codigos validos...')
 
-            let sql_body = '';
-            var amount_valid_lines = 0;
-            var save_times = 0;
-            var index = 0;
+            let lines = {
+                valid_codes: valid_codes_data.recordsets[0],
+                times_saved: 0,
+                actual_sql: [],
+                history_sql: []
+            }
 
             fs.createReadStream(PATH_FILE, 'utf-8')
                 .pipe(csv.parse({ delimiter: ",", from_line: 2 }))
-                .on('data', (line_data) => {
-                    // index++
-                    // if(index == 1){
-                    //     if(row.toString() == utils.file_fields.toString()){
-                    //         console.log('Cabecera de fichero csv no coincide')
-                    //         reject(new Error('uh'));
-                    //     }
-                    // }
+                .on('data', (data) => {                    
+                    utils.buildQueryLineValues(data, lines)
 
-                    // if(row.toString() !== utils.file_fields.toString()){
-                    //     console.log('Cabecera de fichero csv no coincide')
-                    // }else{
-                        sql_body = sql_body + utils.buildQueryLineValues(line_data, valid_codes_data.recordsets[0], amount_valid_lines)
+                    if(lines.actual_sql.length === 500){
+                        let insert_query = sql_head+lines.actual_sql.join(', ');
                         
-                        if(amount_valid_lines == 10){
-                            request.query(sql_head+sql_body.slice(0,-1), (err) => {
-                                save_times++
-                                if(err){
-                                    //console.log(err)
-                                    console.log(save_times + "° : Error save data")   
-                                }                         
-                                console.log(save_times + "° : succesully save data")
-                            })    
-                        }
-                    // }
-                }).on('end', () => {
-                    if(sql_body){
-                        request.query(sql_head+sql_body.slice(0,-1), (err) => {
-                            save_times++
+                        lines.history_sql.push(lines.actual_sql)
+                        lines.actual_sql = []
+                        
+                        request.query(insert_query, (err) => {                                
                             if(err){
-                                //console.log(err)
-                                console.log(save_times + "° : Error save data")   
-                            }                         
-                            console.log(save_times + "° : succesully save data")
+                                utils.writeLog(lines.times_saved, err, lines.history_sql[lines.history_sql.length - 1])
+                                console.log(lines.times_saved + "° : Error save data - " + err)   
+                            } else {
+                            lines.times_saved++                                                  
+                            console.log(lines.times_saved + "° : succesully save data" + utils.getDateTimeString())
+                            }
+                        })  
+                    }
+                }).on('end', () => {
+                    if(lines.actual_sql.length !== 0){
+                        let insert_query = sql_head+lines.actual_sql.join(', ');
+                            
+                        lines.history_sql.push(lines.actual_sql)
+                        lines.actual_sql = []
+
+                        request.query(insert_query, (err) => {                              
+                            if(err){
+                                console.log(lines.times_saved + "° : Error save data - " + err)   
+                            } else {                            
+                                lines.times_saved++
+                                console.log(lines.times_saved + "° : succesully save data - " + utils.getDateTimeString())
+                            }
                         })
                     }
                     console.log('Guardado de datos terminado')
-                    console.log('block number : ' + save_times)
-                    console.log("Tiempo fin proceso : "+utils.getDateTimeString())
+                    console.log('block number : ' + lines.times_saved)
+                    console.log("Tiempo fin proceso : " + utils.getDateTimeString())
             
                 }).on('error', (err) => {
                     console.log(err)
                 })
-
-            //console.log(data.recordset);
         })
     })
-
-    // var cont = 0
-    // var cont2 = 0
-    // var cont3 = 1
-
-    // fs.createReadStream(PATH_FILE, 'utf-8')
-    //     .pipe(es.split())
-    //     .on('data', (row, index) => {
-            
-    //         let data_line = getLineData(row)
-
-    //         file_fields = (row.split(','));
-
-    //         cont = cont + 1
-    //         if(cont3 == 1) {
-    //             console.log(data_line.ANO_EJE)
-    //             cont3 = 0
-    //         }
-    //         if(cont == 5000) {
-    //             // console.log(row)
-    //             cont = 0
-    //             cont2 = cont2 + 1
-    //         }
-
-                     
-    //     }).on('end', () => {
-    //         console.log('finished')
-    //         console.log('block number : ' + cont2)
-    //     })
-}
-
-function getLineData(line){
-    return 
 }
 
 mainFunction()
-
-
-// ANO_EJE,
-// MES_EJE,
-// TIPO_GOBIERNO,
-// TIPO_GOBIERNO_NOMBRE,
-// SECTOR,
-// SECTOR_NOMBRE,
-// PLIEGO,
-// PLIEGO_NOMBRE,
-// SEC_EJEC,
-// EJECUTORA,
-// EJECUTORA_NOMBRE,
-// DEPARTAMENTO_EJECUTORA,
-// DEPARTAMENTO_EJECUTORA_NOMBRE
-// PROVINCIA_EJECUTORA,
-// PROVINCIA_EJECUTORA_NOMBRE,
-// DISTRITO_EJECUTORA,
-// DISTRITO_EJECUTORA_NOMBRE,
-// SEC_FUNC,
-// PROGRAMA_PPTO,
-// PROGRAMA_PPTO_NOMBRE,
-// TIPO_ACT_PROY,
-// PRODUCTO_PROYECTO,
-// PRODUCTO_PROYECTO_NOMBRE,
-// ACTIVIDAD_ACCION_OBRA,
-// ACTIVIDAD_ACCION_OBRA_NOMBRE, 
-// FUNCION,
-// FUNCION_NOMBRE,
-// DIVISION_FUNCIONAL,
-// DIVISION_FUNCIONAL_NOMBRE,
-// GRUPO_FUNCIONAL,
-// GRUPO_FUNCIONAL_NOMBRE,
-// META,
-// FINALIDAD,
-// META_NOMBRE,
-// DEPARTAMENTO_META,
-// DEPARTAMENTO_META_NOMBRE,
-// FUENTE_FINANC,
-// FUENTE_FINANC_NOMBRE,
-// RUBRO,
-// RUBRO_NOMBRE,
-// TIPO_RECURSO,
-// TIPO_RECURSO_NOMBRE,
-// CATEG_GASTO,
-// CATEG_GASTO_NOMBRE,
-// TIPO_TRANSACCION,
-// GENERICA,
-// GENERICA_NOMBRE,
-// SUBGENERICA,
-// SUBGENERICA_NOMBRE,
-// SUBGENERICA_DET,
-// SUBGENERICA_DET_NOMBRE,
-// ESPECIFICA,
-// ESPECIFICA_NOMBRE,
-// ESPECIFICA_DET,
-// ESPECIFICA_DET_NOMBRE,
-// MONTO_PIA,
-// MONTO_PIM,
-// MONTO_CERTIFICADO,
-// MONTO_COMPROMETIDO_ANUAL,
-// MONTO_COMPROMETIDO,
-// MONTO_DEVENGADO,
-// MONTO_GIRADO,
-// TIPO_ACT_PROY_NOMBRE
--- source/utils.js
+++ source/utils.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
     table_fields : [    
         'ANO_EJE',
@@ -12,7 +14,7 @@ module.exports = {
         'EJECUTORA',
         'EJECUTORA_NOMBRE',
         'DEPARTAMENTO_EJECUTORA',
-        'DEPARTAMENTO_EJECUTORA_NOMBR',
+        'DEPARTAMENTO_EJECUTORA_NOMBRE',
         'PROVINCIA_EJECUTORA',
         'PROVINCIA_EJECUTORA_NOMBRE',
         'DISTRITO_EJECUTORA',
@@ -21,10 +23,11 @@ module.exports = {
         'PROGRAMA_PPTO',
         'PROGRAMA_PPTO_NOMBRE',
         'TIPO_ACT_PROY',
+        'TIPO_ACT_PROY_NOMBRE',
         'PRODUCTO_PROYECTO',
         'PRODUCTO_PROYECTO_NOMBRE',
         'ACTIVIDAD_ACCION_OBRA',
-        'ACTIVIDAD_ACCION_OBRA_NOMBRE,',
+        'ACTIVIDAD_ACCION_OBRA_NOMBRE',
         'FUNCION',
         'FUNCION_NOMBRE',
         'DIVISION_FUNCIONAL',
@@ -61,8 +64,7 @@ module.exports = {
         'MONTO_COMPROMETIDO_ANUAL',
         'MONTO_COMPROMETIDO',
         'MONTO_DEVENGADO',
-        'MONTO_GIRADO',
-        'TIPO_ACT_PROY_NOMBR' 
+        'MONTO_GIRADO' 
     ],
     
     file_fields :   [
@@ -131,233 +133,57 @@ module.exports = {
         "MONTO_GIRADO",
       ],
 
-    buildQueryLineValues : (line, valid_codes, amount_valid_lines) => {
-        let code = line[22]
-        let sql_value = ''
+    buildQueryLineValues : (data_line, lines) => {
+        let code = data_line[22]
 
         const valid_code = (value) => value.codigo_cui == code;
 
-        if(valid_codes.some(valid_code)){
-            sql_value = `( "${line.join('", "')}" ),`
-            amount_valid_lines++
+        if(lines.valid_codes.some(valid_code)){
+            lines.actual_sql.push(`( '${data_line.join("', '")}' )`)
         }
-
-
-        return sql_value
     },
+
     getDateTimeString : () => {
         let today = new Date();
-        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        let date = today.getFullYear().toString().padStart(2, '0')+'-'+(today.getMonth()+1).toString().padStart(2, '0')+'-'+today.getDate().toString().padStart(2, '0');
+        let time = today.getHours().toString().padStart(2, '0') + ":" + today.getMinutes().toString().padStart(2, '0') + ":" + today.getSeconds().toString().padStart(2, '0');
         let dateTime = date+' '+time;
         
         return dateTime;
+    },
+
+    updateLineInfo : (sql_head, lines, request) => {
+        let insert_query = sql_head+lines.actual_sql.join(', ');
+                            
+        lines.history_sql.push(lines.actual_sql)
+        lines.actual_sql = []
+
+        request.query(insert_query, (err) => {                                
+            if(err){
+                console.log(lines.times_saved + "° : Error save data - " + err)   
+            } else {
+                lines.times_saved++ 
+                console.log(lines.times_saved + "° : succesully save data" + utils.getDateTimeString())
+            }
+              
+        })
+    },
+
+    writeLog : (times, err, data) => {
+        const timestamp = new Date().toISOString();
+        const log_message = `${timestamp} - ${err}\n`;
+        const log_insert_query = data.join(', \n')
+        const log_data = log_message + log_insert_query
+        const file_name = `logs/${times}-${err}-${timestamp}.log`
+
+        fs.appendFile(file_name, log_data, (err) => {
+            if (err) {
+                console.error('Error al escribir en el archivo de registro de log:', err);
+            }
+        })
     }
-
-
-    // buildQueryLineValues : (line, valid_codes) => {
-    //     let is_valid = valid_codes.find( code => code.codigo_cui == line[]);
-        
-    //     if (is_valid !== undefined){
-    //         return sql_value = `(
-    //             ${ ANO_EJE },
-    //             ${ MES_EJE },
-    //             ${ TIPO_GOBIERNO },
-    //             ${ TIPO_GOBIERNO_NOMBRE },
-    //             ${ SECTOR },
-    //             ${ SECTOR_NOMBRE },
-    //             ${ PLIEGO },
-    //             ${ PLIEGO_NOMBRE },
-    //             ${ SEC_EJEC },
-    //             ${ EJECUTORA },
-    //             ${ EJECUTORA_NOMBRE },
-    //             ${ DEPARTAMENTO_EJECUTORA },
-    //             ${ DEPARTAMENTO_EJECUTORA_NOMBRE },
-    //             ${ PROVINCIA_EJECUTORA },
-    //             ${ PROVINCIA_EJECUTORA_NOMBRE },
-    //             ${ DISTRITO_EJECUTORA },
-    //             ${ DISTRITO_EJECUTORA_NOMBRE },
-    //             ${ SEC_FUNC },
-    //             ${ PROGRAMA_PPTO },
-    //             ${ PROGRAMA_PPTO_NOMBRE },
-    //             ${ TIPO_ACT_PROY },		
-    //             ${ PRODUCTO_PROYECTO },
-    //             ${ PRODUCTO_PROYECTO_NOMBRE },
-    //             ${ ACTIVIDAD_ACCION_OBRA },
-    //             ${ ACTIVIDAD_ACCION_OBRA_NOMBRE },
-    //             ${ FUNCION },
-    //             ${ FUNCION_NOMBRE },
-    //             ${ DIVISION_FUNCIONAL },
-    //             ${ DIVISION_FUNCIONAL_NOMBRE },
-    //             ${ GRUPO_FUNCIONAL },
-    //             ${ GRUPO_FUNCIONAL_NOMBRE },
-    //             ${ META },
-    //             ${ FINALIDAD },
-    //             ${ META_NOMBRE },
-    //             ${ DEPARTAMENTO_META },
-    //             ${ DEPARTAMENTO_META_NOMBRE },
-    //             ${ FUENTE_FINANC },
-    //             ${ FUENTE_FINANC_NOMBRE },
-    //             ${ RUBRO },
-    //             ${ RUBRO_NOMBRE },
-    //             ${ TIPO_RECURSO },
-    //             ${ TIPO_RECURSO_NOMBRE },
-    //             ${ CATEG_GASTO },
-    //             ${ CATEG_GASTO_NOMBRE },
-    //             ${ TIPO_TRANSACCION },
-    //             ${ GENERICA },
-    //             ${ GENERICA_NOMBRE },
-    //             ${ SUBGENERICA },
-    //             ${ SUBGENERICA_NOMBRE },
-    //             ${ SUBGENERICA_DET },
-    //             ${ SUBGENERICA_DET_NOMBRE },
-    //             ${ ESPECIFICA },
-    //             ${ ESPECIFICA_NOMBRE },
-    //             ${ ESPECIFICA_DET },
-    //             ${ ESPECIFICA_DET_NOMBRE },
-    //             ${ MONTO_PIA },
-    //             ${ MONTO_PIM },
-    //             ${ MONTO_CERTIFICADO },
-    //             ${ MONTO_COMPROMETIDO_ANUAL },
-    //             ${ MONTO_COMPROMETIDO },
-    //             ${ MONTO_DEVENGADO },
-    //             ${ MONTO_GIRADO },
-    //             ${ TIPO_ACT_PROY_NOMBRE },
-    //         ),`
-    //     } else {
-    //         return '';
-    //     }
-    // }
 
 }
 
-
-// ${ ANO_EJE },
-// ${ MES_EJE },
-// ${ TIPO_GOBIERNO },
-// ${ TIPO_GOBIERNO_NOMBRE },
-// ${ SECTOR },
-// ${ SECTOR_NOMBRE },
-// ${ PLIEGO },
-// ${ PLIEGO_NOMBRE },
-// ${ SEC_EJEC },
-// ${ EJECUTORA },
-// ${ EJECUTORA_NOMBRE },
-// ${ DEPARTAMENTO_EJECUTORA },
-// ${ DEPARTAMENTO_EJECUTORA_NOMBRE },
-// ${ PROVINCIA_EJECUTORA },
-// ${ PROVINCIA_EJECUTORA_NOMBRE },
-// ${ DISTRITO_EJECUTORA },
-// ${ DISTRITO_EJECUTORA_NOMBRE },
-// ${ SEC_FUNC },
-// ${ PROGRAMA_PPTO },
-// ${ PROGRAMA_PPTO_NOMBRE },
-// ${ TIPO_ACT_PROY },		
-// ${ PRODUCTO_PROYECTO },
-// ${ PRODUCTO_PROYECTO_NOMBRE },
-// ${ ACTIVIDAD_ACCION_OBRA },
-// ${ ACTIVIDAD_ACCION_OBRA_NOMBRE },
-// ${ FUNCION },
-// ${ FUNCION_NOMBRE },
-// ${ DIVISION_FUNCIONAL },
-// ${ DIVISION_FUNCIONAL_NOMBRE },
-// ${ GRUPO_FUNCIONAL },
-// ${ GRUPO_FUNCIONAL_NOMBRE },
-// ${ META },
-// ${ FINALIDAD },
-// ${ META_NOMBRE },
-// ${ DEPARTAMENTO_META },
-// ${ DEPARTAMENTO_META_NOMBRE },
-// ${ FUENTE_FINANC },
-// ${ FUENTE_FINANC_NOMBRE },
-// ${ RUBRO },
-// ${ RUBRO_NOMBRE },
-// ${ TIPO_RECURSO },
-// ${ TIPO_RECURSO_NOMBRE },
-// ${ CATEG_GASTO },
-// ${ CATEG_GASTO_NOMBRE },
-// ${ TIPO_TRANSACCION },
-// ${ GENERICA },
-// ${ GENERICA_NOMBRE },
-// ${ SUBGENERICA },
-// ${ SUBGENERICA_NOMBRE },
-// ${ SUBGENERICA_DET },
-// ${ SUBGENERICA_DET_NOMBRE },
-// ${ ESPECIFICA },
-// ${ ESPECIFICA_NOMBRE },
-// ${ ESPECIFICA_DET },
-// ${ ESPECIFICA_DET_NOMBRE },
-// ${ MONTO_PIA },
-// ${ MONTO_PIM },
-// ${ MONTO_CERTIFICADO },
-// ${ MONTO_COMPROMETIDO_ANUAL },
-// ${ MONTO_COMPROMETIDO },
-// ${ MONTO_DEVENGADO },
-// ${ MONTO_GIRADO },
-// ${ TIPO_ACT_PROY_NOMBRE },
-
-// [ANO_EJE,
-// MES_EJE,
-// TIPO_GOBIERNO,
-// TIPO_GOBIERNO_NOMBRE,
-// SECTOR,
-// SECTOR_NOMBRE,
-// PLIEGO,
-// PLIEGO_NOMBRE,
-// SEC_EJEC,
-// EJECUTORA,
-// EJECUTORA_NOMBRE,
-// DEPARTAMENTO_EJECUTORA,
-// DEPARTAMENTO_EJECUTORA_NOMBRE
-// PROVINCIA_EJECUTORA,
-// PROVINCIA_EJECUTORA_NOMBRE,
-// DISTRITO_EJECUTORA,
-// DISTRITO_EJECUTORA_NOMBRE,
-// SEC_FUNC,
-// PROGRAMA_PPTO,
-// PROGRAMA_PPTO_NOMBRE,
-// TIPO_ACT_PROY,
-// PRODUCTO_PROYECTO,
-// PRODUCTO_PROYECTO_NOMBRE,
-// ACTIVIDAD_ACCION_OBRA,
-// ACTIVIDAD_ACCION_OBRA_NOMBRE, 
-// FUNCION,
-// FUNCION_NOMBRE,
-// DIVISION_FUNCIONAL,
-// DIVISION_FUNCIONAL_NOMBRE,
-// GRUPO_FUNCIONAL,
-// GRUPO_FUNCIONAL_NOMBRE,
-// META,
-// FINALIDAD,
-// META_NOMBRE,
-// DEPARTAMENTO_META,
-// DEPARTAMENTO_META_NOMBRE,
-// FUENTE_FINANC,
-// FUENTE_FINANC_NOMBRE,
-// RUBRO,
-// RUBRO_NOMBRE,
-// TIPO_RECURSO,
-// TIPO_RECURSO_NOMBRE,
-// CATEG_GASTO,
-// CATEG_GASTO_NOMBRE,
-// TIPO_TRANSACCION,
-// GENERICA,
-// GENERICA_NOMBRE,
-// SUBGENERICA,
-// SUBGENERICA_NOMBRE,
-// SUBGENERICA_DET,
-// SUBGENERICA_DET_NOMBRE,
-// ESPECIFICA,
-// ESPECIFICA_NOMBRE,
-// ESPECIFICA_DET,
-// ESPECIFICA_DET_NOMBRE,
-// MONTO_PIA,
-// MONTO_PIM,
-// MONTO_CERTIFICADO,
-// MONTO_COMPROMETIDO_ANUAL,
-// MONTO_COMPROMETIDO,
-// MONTO_DEVENGADO,
-// MONTO_GIRADO,
-// TIPO_ACT_PROY_NOMBRE]
 
 
